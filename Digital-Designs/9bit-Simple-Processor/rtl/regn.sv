04256c2dab872399c120974a92ba78ba99f67817
asuslenovotufroxtric <adatt854@gmail.com>
2026-06-29 23:21:46 +0700

    Update regn.sv

--- Digital-Designs/9bit-Simple-Processor/rtl/regn.sv
+++ Digital-Designs/9bit-Simple-Processor/rtl/regn.sv
@@ -1,1 +1,10 @@
 
+module regn #(parameter n = 9) (
+    input  logic [n-1:0] R,
+    input  logic Rin, Clock,
+    output logic [n-1:0] Q
+);
+    always_ff @(posedge Clock) begin
+        if (Rin) Q <= R;
+    end
+endmodule
