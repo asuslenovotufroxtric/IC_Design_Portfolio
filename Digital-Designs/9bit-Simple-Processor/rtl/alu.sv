d114e3149c7f2dfc1b473f822de35e7c6c55e0f0
asuslenovotufroxtric <adatt854@gmail.com>
2026-06-29 23:20:41 +0700

    Update alu.sv

--- Digital-Designs/9bit-Simple-Processor/rtl/alu.sv
+++ Digital-Designs/9bit-Simple-Processor/rtl/alu.sv
@@ -1,1 +1,10 @@
-
+module alu (
+input logic [8:0] A, B,
+input logic Sub ,
+output logic [8:0] S
+        );
+ always_comb begin
+ if ( Sub ) S = A - B;
+       else S = A + B;
+            end
+endmodule
